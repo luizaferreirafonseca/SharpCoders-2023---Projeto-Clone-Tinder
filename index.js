6519efa1e281746362ad0c80e8709ea84924adb5
Luiza Ferreira <luiza.ferreira.fonseca@gmail.com>
2023-11-23 16:14:15 -0300

    button X

---
+++ index.js
@@ -0,0 +1,8 @@
+const buttonX = () => {
+    const mainImage = document.querySelector('.photo');
+    const nextPerson = document.querySelector('.next_photo')
+    mainImage.style.display = 'none'; 
+    nextPerson.style.display = 'flex'
+
+    
+}
